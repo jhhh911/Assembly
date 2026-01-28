d03c4921e5c24ea7c59e209843d720dde734569d
James Heiniger <james.heiniger911@gmail.com>
2026-01-28 17:56:46 +1030

    Added language chips

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
 import React from "react";
+import { languages } from "./languages";
 
 export default function Hangman() {
+  const languageList = languages.map(language => {
+    const styles = {
+      backgroundColor: language.backgroundColor,
+      color: language.color,
+    };
+    return (
+      <div style={styles} className="chips" key={language.name}>
+        {language.name}
+      </div>
+    );
+  });
   return (
     <main>
       <header>
@@ -15,6 +27,7 @@ export default function Hangman() {
         <h2>You win!</h2>
         <p>Well done! 🎉</p>
       </section>
+      <section className="language-chips">{languageList}</section>
     </main>
   );
 }
