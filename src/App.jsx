31fb35f4a6899d925af8431770bd51972ed66b76
James Heiniger <james.heiniger911@gmail.com>
2026-01-28 16:15:29 +1030

    Styled winning status

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,19 @@ import React from "react";
 
 export default function Hangman() {
   return (
-    <header>
-      <h1>Assembly: Endgame</h1>
-      <p>
-        Guess the word in under 8 attempts to keep the <br />
-        programming world safe from Assembly!
-      </p>
-    </header>
+    <main>
+      <header>
+        <h1>Assembly: Endgame</h1>
+        <p>
+          Guess the word in under 8 attempts to keep the <br />
+          programming world safe from Assembly!
+        </p>
+      </header>
+
+      <section className="status">
+        <h2>You win!</h2>
+        <p>Well done! 🎉</p>
+      </section>
+    </main>
   );
 }
