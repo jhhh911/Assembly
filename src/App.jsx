99c8c083c498c969024d0e3ebe56ff8006e67e15
James Heiniger <james.heiniger911@gmail.com>
2026-01-28 15:49:03 +1030

    Completed header section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import React from "react"
+import React from "react";
 
 export default function Hangman() {
-
   return (
-    <main>
-      Game goes here
-    </main>
-  )
+    <header>
+      <h1>Assembly: Endgame</h1>
+      <p>
+        Guess the word in under 8 attempts to keep the <br />
+        programming world safe from Assembly!
+      </p>
+    </header>
+  );
 }
-
